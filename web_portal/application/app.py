9088a47ec9d4ac12824b303548a81e587a8b7d15
Sanket <sanketpatil7467@gmail.com>
2024-01-04 08:17:55 +0530

    employee registration added

--- web_portal/application/app.py
+++ web_portal/application/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+import json
 app = Flask(__name__)
+
 
 
 @app.route('/')
@@ -14,7 +16,24 @@ def admin_login():
 
 @app.route("/register_user", methods=['POST'])
 def register_user():
-    return render_template('register.html')
+    fname = request.form['fname']
+    lname = request.form['lname']
+    email = request.form['email']
+    password = request.form['password']
+
+    if password == "admin":
+        file_path = 'employee_db.txt'
+        with open(file_path, 'r') as file:
+            loaded_dict = json.load(file)
+
+        if email in loaded_dict.keys():
+            # Employee already exists
+            pass
+        else:
+            loaded_dict[email] = {"fname": fname, "lname":lname}
+        with open(file_path, 'w') as convert_file:
+            convert_file.write(json.dumps(loaded_dict))
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
