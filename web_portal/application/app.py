dde731d265bff1aaba9e337acc92e7d4ef001dba
Sanket <sanketpatil7467@gmail.com>
2024-01-06 14:17:53 +0530

    admin_half_complete

--- web_portal/application/app.py
+++ web_portal/application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect,url_for
 import json
 app = Flask(__name__)
 
@@ -11,29 +11,52 @@ def home_page_login():
 
 @app.route("/admin_login", methods=['POST'])
 def admin_login():
-    return render_template('register.html')
+    return render_template('admin_login.html')
+
+
+@app.route("/add_employee", methods=['POST'])
+def addEmployee():
+    return render_template('employers_registration.html')
+    # redirect(url_for('user_login.html'))
+
+
+@app.route("/back_to_admin_dashboard", methods=['POST'])
+def backToAdminDashboard():
+    return render_template('admin_dashboard.html')
 
 
 @app.route("/register_user", methods=['POST'])
-def register_user():
+def registerUser():
+    success_status = "Employee Successfully added"
     fname = request.form['fname']
     lname = request.form['lname']
     email = request.form['email']
+
+    file_path = 'employee_db.txt'
+    with open(file_path, 'r') as file:
+        loaded_dict = json.load(file)
+
+    if email in loaded_dict.keys():
+        success_status = "Employee already exists."
+        pass
+    else:
+        loaded_dict[email] = {"fname": fname, "lname":lname}
+    with open(file_path, 'w') as convert_file:
+        convert_file.write(json.dumps(loaded_dict))
+    return render_template('employers_registration.html',status = success_status)
+
+
+
+
+@app.route("/validate_admin", methods=['POST'])
+def validateAdmin():
+    status ="Invalid Password"
     password = request.form['password']
-
     if password == "admin":
-        file_path = 'employee_db.txt'
-        with open(file_path, 'r') as file:
-            loaded_dict = json.load(file)
+        return render_template('admin_dashboard.html')
+    else:
+        return render_template('admin_login.html',status=status)
 
-        if email in loaded_dict.keys():
-            # Employee already exists
-            pass
-        else:
-            loaded_dict[email] = {"fname": fname, "lname":lname}
-        with open(file_path, 'w') as convert_file:
-            convert_file.write(json.dumps(loaded_dict))
-    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
